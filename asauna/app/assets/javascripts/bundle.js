d0832038e31e2b55a1bcf882e67450a5b5f641ff
eightseconds <paulseokcho@gmail.com>
2019-07-10 01:11:40 -0700

    Implement splash css

--- asauna/app/assets/javascripts/bundle.js
+++ asauna/app/assets/javascripts/bundle.js
@@ -416,8 +416,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_modal_modal__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../components/modal/modal */ "./frontend/components/modal/modal.jsx");
 /* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
 /* harmony import */ var _create_workspace_page_create_workspace_page_container__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ./create_workspace_page/create_workspace_page_container */ "./frontend/components/create_workspace_page/create_workspace_page_container.js");
-/* harmony import */ var _project_loading_project_loading_container__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(/*! ./project_loading/project_loading_container */ "./frontend/components/project_loading/project_loading_container.js");
-
 
 
 
@@ -443,9 +441,6 @@ var App = function App(_ref) {
   }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
     path: "/home/loading",
     component: _loading_page_loading_page_container__WEBPACK_IMPORTED_MODULE_7__["default"]
-  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
-    path: "/home/projectloading",
-    component: _project_loading_project_loading_container__WEBPACK_IMPORTED_MODULE_11__["ProjectLoadingContainer"]
   }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Route"], {
     path: "/home/create",
     component: _create_workspace_page_create_workspace_page_container__WEBPACK_IMPORTED_MODULE_10__["default"]
@@ -945,20 +940,44 @@ function (_React$Component) {
     key: "componentDidMount",
     value: function componentDidMount() {
       var that = this;
-      this.props.fetchWorkspaces().then(function () {
+      this.props.fetchWorkspaces().then(setTimeout(function () {
         if (!!that.props.workspaces.length) {
           that.props.history.push("/home/".concat(that.props.workspaces[that.props.workspaces.length - 1].id));
         } else {
           that.props.history.push('/home/create');
         }
-      });
+      }, 2000));
     }
   }, {
     key: "render",
     value: function render() {
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {
-        className: "fas fa-spinner"
-      }));
+      var stylesObj1 = {
+        background: "rgba(255, 82, 99, 0.9)"
+      };
+      var stylesObj2 = {
+        background: "rgba(255, 115, 129, 0.9)"
+      };
+      var stylesObj3 = {
+        background: "rgba(252, 189, 1, 0.9)"
+      };
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "spinner"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "bounce1",
+        style: stylesObj1
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "bounce2",
+        style: stylesObj2
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "bounce3",
+        style: stylesObj3
+      })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "loadinglogo"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("i", {
+        className: "fas fa-fire"
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "session--logo"
+      }, "asauna")));
     }
   }]);
 
@@ -1813,106 +1832,6 @@ function (_React$Component) {
 }(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
 
 /* harmony default export */ __webpack_exports__["default"] = (Object(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["withRouter"])(Project));
-
-/***/ }),
-
-/***/ "./frontend/components/project_loading/project_loading.jsx":
-/*!*****************************************************************!*\
-  !*** ./frontend/components/project_loading/project_loading.jsx ***!
-  \*****************************************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-
-
-
-var ProjectLoading =
-/*#__PURE__*/
-function (_React$Component) {
-  _inherits(ProjectLoading, _React$Component);
-
-  function ProjectLoading(props) {
-    _classCallCheck(this, ProjectLoading);
-
-    return _possibleConstructorReturn(this, _getPrototypeOf(ProjectLoading).call(this, props));
-  }
-
-  _createClass(ProjectLoading, [{
-    key: "componentDidMount",
-    value: function componentDidMount() {
-      var that = this;
-      this.props.fetchProjects(this.props.match.params.workspaceId).then(function () {
-        that.props.history.push("/home/".concat(that.props.match.params.workspaceId));
-      });
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, "Loading");
-    }
-  }]);
-
-  return ProjectLoading;
-}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
-
-/* harmony default export */ __webpack_exports__["default"] = (Object(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["withRouter"])(ProjectLoading));
-
-/***/ }),
-
-/***/ "./frontend/components/project_loading/project_loading_container.js":
-/*!**************************************************************************!*\
-  !*** ./frontend/components/project_loading/project_loading_container.js ***!
-  \**************************************************************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
-/* harmony import */ var _actions_project_actions__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../actions/project_actions */ "./frontend/actions/project_actions.js");
-/* harmony import */ var _project_loading__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./project_loading */ "./frontend/components/project_loading/project_loading.jsx");
-
-
-
-
-var mapStateToProps = function mapStateToProps(state, ownProps) {
-  return {
-    projects: Object.values(state.entities.projects)
-  };
-};
-
-var mapDispatchToProps = function mapDispatchToProps(dispatch) {
-  return {
-    fetchProjects: function fetchProjects(workspaceId) {
-      return dispatch(Object(_actions_project_actions__WEBPACK_IMPORTED_MODULE_1__["fetchProjects"])(workspaceId));
-    }
-  };
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_0__["connect"])(mapStateToProps, mapDispatchToProps)(_project_loading__WEBPACK_IMPORTED_MODULE_2__["default"]));
 
 /***/ }),
 
--- asauna/frontend/components/loading_page/loading_page.jsx
+++ asauna/frontend/components/loading_page/loading_page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { withRouter } from 'react-router-dom';
 
+
 class LoadingPage extends React.Component {
     constructor(props) {
         super(props)
@@ -23,21 +24,40 @@ class LoadingPage extends React.Component {
     componentDidMount(){
         let that = this;
         this.props.fetchWorkspaces()
-        .then(() => {
+        .then(setTimeout(() => {
             if (!!that.props.workspaces.length) {
                 that.props.history.push(`/home/${that.props.workspaces[that.props.workspaces.length - 1].id}`)
             } else {
                 that.props.history.push('/home/create')
             }
-        })
+        }, 2000)
+        )
     }
 
 
 
     render() {
+        const stylesObj1 = {
+            background: "rgba(255, 82, 99, 0.9)"
+        }
+        const stylesObj2 = {
+            background: "rgba(255, 115, 129, 0.9)"
+        }
+        const stylesObj3 = {
+            background: "rgba(252, 189, 1, 0.9)"
+        }
+
         return (
-            <div>   
-                <i className="fas fa-spinner" ></i>
+            <div>
+                <div className="spinner">
+                    <div className="bounce1" style={stylesObj1}></div>
+                    <div className="bounce2" style={stylesObj2}></div>
+                    <div className="bounce3" style={stylesObj3}></div>
+                </div>
+                <div className="loadinglogo">
+                    <i className="fas fa-fire"></i>
+                    <div className="session--logo">asauna</div>
+                </div>
             </div>
         )
     }
